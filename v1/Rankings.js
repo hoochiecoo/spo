761b589d5bbcab9bc105e06f9a1ad8af2224f0cc
Anatoly <waveinspectro@gmail.com>
2024-06-14 12:54:38 +0200

    Update Rankings.js

--- v1/Rankings.js
+++ v1/Rankings.js
@@ -1,9 +1,23 @@
 // Define Content component
 function Content() {
-    return React.createElement('div', null, 'Hello, Rankings');
+    return React.createElement('iframe', {
+        title: "SUBOTICA BOWLING 2024",
+        'aria-label': "Table",
+        id: "datawrapper-chart-5vcmO",
+        src: "https://datawrapper.dwcdn.net/5vcmO/15/",
+        scrolling: "no",
+        frameBorder: "0",
+        style: {
+            width: '100%',
+            minWidth: '100% !important',
+            border: 'none'
+        },
+        height: 507,
+        'data-external': 1
+    });
 }
 
-// Render the Content component into the 'content' container
+// Render the Content component into the 'Rankings' container
 ReactDOM.render(
     React.createElement(Content),
     document.getElementById('Rankings')
