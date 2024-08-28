37514337d760458867252e8a0fc9975958ecfd0f
Anatoly <waveinspectro@gmail.com>
2024-08-28 13:48:33 +0200

    Update and rename v3/Content.js to v3/content-folder/Content.js

---
+++ v3/content-folder/Content.js
@@ -0,0 +1,12 @@
+// content-folder/Content.js
+
+// Import CSS file
+import './Content.css';
+
+// Define Content component
+function Content() {
+    return React.createElement('div', { className: 'content' }, 'Hello, Home');
+}
+
+// Export Content component
+export default Content;
